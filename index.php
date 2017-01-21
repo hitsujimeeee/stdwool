645ec968a34f0f1355a0fb57bfe05535ac8e2d69
hitsujimeeee <hitsuji_sw@yahoo.co.jp>
2017-01-21 22:18:47 +0900

    プロジェクトコミット

---
+++ index.php
@@ -0,0 +1,35 @@
+
+<!DOCTYPE html>
+<html lang="ja">
+
+	<head>
+		<meta charset="UTF-8" />
+		<meta name="author" content="Studio Wool">
+		<meta name="description" content="同人ゲーム制作サークル Studio Wool のホームページです。フリーのゲームを配布しています。">
+		<title>同人ゲームサークル Studio Wool HP</title>
+		<link rel="stylesheet" href="./css/common.css">
+		<link rel="stylesheet" href="./css/index.css">
+	</head>
+
+	<body>
+		<?php include_once("./php/analyticstracking.php") ?>
+		<main>
+			<div class="siteName">同人ゲームサークル <strong>Studio Wool</strong> ホームページ</div>
+			<div class="mascot">
+				<a href="html/top.html"><img src="img/index.png"></a>
+			</div>
+			<div class="enterLink">
+				<a href="html/top.html">Enter</a>
+			</div>
+
+		</main>
+
+
+		<!-- フッター -->
+		<footer>
+			Copyright (C) 2009-2012 <a href="../">Studio Wool</a> All Rights Reserved.
+		</footer>
+
+	</body>
+
+</html>
