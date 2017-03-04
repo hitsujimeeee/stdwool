8664e970a132090ff6cf35519d83d856ac99b2d0
hitsujimeeee <hitsuji_sw@yahoo.co.jp>
2017-03-04 17:31:08 +0900

    マニュアルにデッキシェア機能追加、目次追加

--- products/pawapro/php/manual.php
+++ products/pawapro/php/manual.php
@@ -13,17 +13,49 @@
 
 	<main>
 		<header class="pageHeader">
-			<h2 id="title"><i class="fa fa-home"></i>パワプロアプリ育成シミュレーター 使い方</h2>
+			<h2 id="title"><i class="fa fa-book"></i>パワプロアプリ育成シミュレーター 使い方</h2>
 		</header>
 
 		<section>
-			<h3>■育成シミュレーターの使い方</h3>
+			<h3 class="contentHeader">■目次</h3>
+			<ul>
+				<li><a href="#idx1">育成シミュレーターの使い方</a>
+					<ul>
+						<li><a href="#idx1-1">各タブの役割</a></li>
+						<li><a href="#idx1-2">能力の入力方法</a></li>
+						<li><a href="#idx1-3">特殊能力の選択方法</a></li>
+						<li><a href="#idx1-4">経験点の算出方法</a></li>
+						<li><a href="#idx1-5">査定最大化機能</a></li>
+						<li><a href="#idx1-6">保存機能</a></li>
+						<li><a href="#idx1-7">選手画像のアップロード</a></li>
+						<li><a href="#idx1-8">選手情報の共有</a></li>
+					</ul>
+				</li>
+				<li><a href="#idx2">デッキシェア機能の使い方</a>
+					<ul>
+						<li><a href="#idx2-1">機能説明</a></li>
+						<li><a href="#idx2-2">デッキを作る</a></li>
+						<li><a href="#idx2-3">デッキを登録する</a></li>
+						<li><a href="#idx2-4">作成済みデッキを編集する</a></li>
+						<li><a href="#idx2-5">デッキを探す</a></li>
+						<li><a href="#idx2-6">デッキをお気に入り登録する</a></li>
+						<li><a href="#idx2-7">デッキを共有する</a></li>
+					</ul>
+				</li>
+			</ul>
+			<hr class="abHr">
+		</section>
+
+
+
+		<section>
+			<h3 class="contentHeader" id="idx1">■育成シミュレーターの使い方</h3>
 
 			<section>
 				<div>
-					<img src="../img/manual/001.jpg">
+					<img src="../img/manual/01-01-01.jpg">
 				</div>
-				<h3 class="sectionHeader"><i class="fa fa-pencil"></i>各タブの役割</h3>
+				<h3 id="idx1-1" class="sectionHeader"><i class="fa fa-pencil"></i>各タブの役割</h3>
 				<dl class="descDefList">
 					<dt>①現在値</dt>
 					<dd>選手の現在のステータスを入力する部分です。</dd>
@@ -35,7 +67,7 @@
 			</section>
 
 			<section>
-				<h3 class="sectionHeader"><i class="fa fa-mouse-pointer"></i>各能力の入力方法</h3>
+				<h3 id="idx1-2" class="sectionHeader"><i class="fa fa-mouse-pointer"></i>各能力の入力方法</h3>
 				<dl class="descDefList">
 					<dt>・基礎能力</dt>
 					<dd>
@@ -59,16 +91,16 @@
 			</section>
 
 			<section>
-				<h3 class="sectionHeader"><i class="fa fa-hand-pointer-o"></i>特殊能力の選択方法</h3>
-				<div><img src="../img/manual/002.jpg"></div>
+				<h3 id="idx1-3" class="sectionHeader"><i class="fa fa-hand-pointer-o"></i>特殊能力の選択方法</h3>
+				<div><img src="../img/manual/01-03-01.jpg"></div>
 				<p>現在値タブ、目標値タブで特殊能力の「追加」ボタンを押すと、特殊能力リストが表示されます。</p>
-				<div><img src="../img/manual/003.jpg"></div>
+				<div><img src="../img/manual/01-03-02.jpg"></div>
 				<p>特殊能力リスト上で習得したい特殊能力をクリックすると、特能選択ウインドウが表示されます。</p>
-				<div><img src="../img/manual/004.jpg"></div>
+				<div><img src="../img/manual/01-03-03.jpg"></div>
 				<p>特殊能力、コツを設定してOKボタンを押します。</p>
-				<div><img src="../img/manual/005.jpg"></div>
+				<div><img src="../img/manual/01-03-04.jpg"></div>
 				<p>特殊能力リストに選択した内容が反映されます。</p>
-				<div><img src="../img/manual/006.jpg"></div>
+				<div><img src="../img/manual/01-03-05.jpg"></div>
 				<p>
 					特殊能力リスト左上の×ボタン、もしくは下部のOKボタンをクリックすると、特殊能力リスト画面を閉じます。<br>
 					選択した特殊能力の一覧が特殊能力欄に、表示されます。
@@ -76,51 +108,51 @@
 			</section>
 
 			<section>
-				<h3 class="sectionHeader"><i class="fa fa-calculator"></i>経験点の算出方法</h3>
-				<div><img src="../img/manual/007.jpg"></div>
-				<div><img src="../img/manual/008.jpg"></div>
+				<h3 id="idx1-4" class="sectionHeader"><i class="fa fa-calculator"></i>経験点の算出方法</h3>
+				<div><img src="../img/manual/01-04-01.jpg"></div>
+				<div><img src="../img/manual/01-04-02.jpg"></div>
 				<p>現在値と目標値を設定したら、確認画面タブに移動します。</p>
-				<div><img src="../img/manual/009.jpg"></div>
+				<div><img src="../img/manual/01-04-03.jpg"></div>
 				<p>経験点算出ボタンをクリックします。</p>
-				<div><img src="../img/manual/010.jpg"></div>
+				<div><img src="../img/manual/01-04-04.jpg"></div>
 				<p>現在値から目標値に達するまでに必要な経験点が表示されます。</p>
 			</section>
 
-			<section>
+			<section id="idx1-5">
 				<h3 class="sectionHeader"><img class="iconGraph" src="../img/icon/muscle.png">査定最大化機能</h3>
-				<div><img src="../img/manual/011.jpg"></div>
+				<div><img src="../img/manual/01-05-01.jpg"></div>
 				<p>現在値タブで現在の能力、各習得済みコツを設定します。</p>
-				<div><img src="../img/manual/012.jpg"></div>
+				<div><img src="../img/manual/01-05-02.jpg"></div>
 				<p>確認画面タブの査定最大化欄に所持経験点を入力し、「実行」ボタンをクリックします。</p>
-				<div><img src="../img/manual/013.jpg"></div>
+				<div><img src="../img/manual/01-05-03.jpg"></div>
 				<p>
 					査定最大化処理が行われ、目標値タブに査定を最大化した場合のステータスが表示されます。<br>
 					※入力内容によっては、処理に時間がかかる場合があります。
 				</p>
 			</section>
 			<section id="saveSection">
-				<h3 class="sectionHeader"><i class="fa fa-floppy-o"></i>保存機能</h3>
+				<h3 id="idx1-6" class="sectionHeader"><i class="fa fa-floppy-o"></i>保存機能</h3>
 				<h4>●入力した選手情報を保存する</h4>
 				<p>
 					画面右下の「<i class="fa fa-gear"></i>設定」ボタンをクリックし、設定ウインドウを開きます。<br>
 				</p>
-				<div><img src="../img/manual/06-01.jpg"></div>
+				<div><img src="../img/manual/01-06-01.jpg"></div>
 				<p>
 					ユーザー名、パスワードを入力し「OK」ボタンをクリックします。<br>
 					<span style="color:#ff0000;">※ユーザー登録の必要はありません。ユーザー名とパスワードの組み合わせに紐づけて選手データが保存されます。<br>　他の人と被る事が無いようなる複雑なパスワードを設定してください。</span><br>
 					ユーザー名、パスワードはともに半角英数字で8～20文字にしてください。<br>
 				</p>
-				<div><img src="../img/manual/06-02.jpg"></div>
+				<div><img src="../img/manual/01-06-02.jpg"></div>
 				<p>
 					確認画面タブ最下部の「保存」ボタンをクリックします。
 				</p>
-				<div><img src="../img/manual/06-03.jpg"></div>
+				<div><img src="../img/manual/01-06-03.jpg"></div>
 				<p>作成した選手のデータが保存されます。</p>
-				<div><img src="../img/manual/06-04.jpg"></div>
+				<div><img src="../img/manual/01-06-04.jpg"></div>
 				<h4>●保存した選手情報を表示する</h4>
 				<p>トップ画面で「作成選手一覧」リンクをクリックします。</p>
-				<div><img src="../img/manual/06-05.jpg"></div>
-				<div><img src="../img/manual/06-06.jpg"></div>
+				<div><img src="../img/manual/01-06-05.jpg"></div>
+				<div><img src="../img/manual/01-06-06.jpg"></div>
 				<p>
 					「<i class="fa fa-gear"></i>設定」ウインドウに入力済みのユーザー名、パスワードで登録した選手情報の一覧で表示されます。<br>
 					編集ボタンを押すと選手情報の編集画面へと遷移します。
@@ -128,26 +160,26 @@
 			</section>
 
 			<section>
-				<h3 class="sectionHeader"><i class="fa fa-link"></i>選手画像のアップロード</h3>
+				<h3 id="idx1-7" class="sectionHeader"><i class="fa fa-link"></i>選手画像のアップロード</h3>
 				<p>
 					登録した選手にお好みの画像を設定できます。<br><br>
 					「確認画面」タブ内の選手情報にある画像の部分をクリックします。
 				</p>
-				<div><img src="../img/manual/07-01.jpg"></div>
+				<div><img src="../img/manual/01-07-01.jpg"></div>
 				<p>
 					PC(スマフォ)内の画像を選択します
 				</p>
-				<div><img src="../img/manual/07-02.jpg"></div>
+				<div><img src="../img/manual/01-07-02.jpg"></div>
 				<p>
 					選択した画像が選手の画像として使用されます。<br>
 					「保存」ボタンを押すと登録が完了し、次回以降も選択した画像で表示されます。
 				</p>
-				<div><img src="../img/manual/07-03.jpg"></div>
+				<div><img src="../img/manual/01-07-03.jpg"></div>
 			</section>
 
 			<section>
-				<h3 class="sectionHeader"><i class="fa fa-link"></i>選手情報の共有</h3>
-				<div><img src="../img/manual/018.jpg"></div>
+				<h3 id="idx1-8" class="sectionHeader"><i class="fa fa-link"></i>選手情報の共有</h3>
+				<div><img src="../img/manual/01-08-01.jpg"></div>
 				<p>
 					作成選手一覧画面から選手情報の編集を押して表示される画面のURLを使用することで、他のユーザーに選手情報を共有できます。<br>
 					他のユーザーが共有URLから選手情報を参照し、選手情報を変更した場合でも、共有元の選手情報が更新されることはありません。<br>
@@ -156,6 +188,167 @@
 			</section>
 		</section>
 
+		<section>
+			<h3 class="contentHeader" id="idx2">■デッキシェア機能の使い方</h3>
+			<section>
+				<h3 id="idx2-1" class="sectionHeader"><i class="fa fa-info-circle"></i>機能説明</h3>
+				<div><img src="../img/manual/02-01-01.jpg"></div>
+				<p>
+					デッキシェア機能では、自分の考えたデッキを他のユーザーに公開したり、他のユーザーが考えたデッキを見たりすることができます。<br>
+					検索機能を使って様々な条件でデッキを検索したり、お気に入り機能で興味のあるデッキをブックマークする事ができます。
+				</p>
+			</section>
+
+			<section>
+				<h3 id="idx2-2" class="sectionHeader"><i class="fa fa-wrench"></i>デッキを作る</h3>
+				<p>
+					<a href="./deckShare.php">デッキシェアページ</a>から、<i class="fa fa-wrench"></i>作るボタンをクリックします。<br>
+				</p>
+				<div><img src="../img/manual/02-02-01.jpg"></div>
+				<p>
+					新規作成ボタンをクリックします。<br>
+				</p>
+				<div><img src="../img/manual/02-02-02.jpg"></div>
+				<p>
+					デッキ作成画面へ移動します。<br>
+					画面上の入力可能項目を入力していきます。
+				</p>
+				<div><img src="../img/manual/02-02-03.jpg"></div>
+				<p class="dlTitle">▼入力可能項目</p>
+				<dl class="defList">
+					<dt>デッキ名</dt>
+					<dd>30文字以内</dd>
+					<dt>イベキャラ</dt>
+					<dd>
+						編集ボタンからイベキャラ一覧ウインドウを開き、デッキにセットしたいイベキャラを選択します。<br>
+						Lvとレアリティも入力できます。
+					</dd>
+					<dt>育成タイプ</dt>
+					<dd>このデッキを使って育成する選手のタイプを設定します。</dd>
+					<dt>対象高校</dt>
+					<dd>どの高校での育成に適しているかを設定します。</dd>
+					<dt>特徴・コンセプト</dt>
+					<dd>このデッキの特徴、長所や注意点などを500文字以内で自由に記述できます。</dd>
+					<dt>作成選手一覧</dt>
+					<dd>
+						このデッキを使用して作成した選手を設定できます。<br>
+						選手の登録の方法は<a href="#idx1-6">入力した選手情報を保存する</a>を参照してください。
+					</dd>
+					<dt>作者情報</dt>
+					<dd>
+						作者名、パワプロID,TwitterID等を入力できます。<br>
+						画面右下の<i class="fa fa-gear"></i>設定ボタンから入力できるユーザー情報にすでにパワプロID,TwitterIDを入力済みの場合、その情報が自動で反映されます。<br>
+						TwitterIDは@を抜いたものを記入して下さい。
+					</dd>
+				</dl>
+			</section>
+
+			<section>
+				<h3 id="idx2-3" class="sectionHeader"><i class="fa fa-wrench"></i>デッキを登録する</h3>
+				<p>
+					デッキ作成画面下部の保存ボタンをクリックします。
+					※保存機能を利用するには、ユーザー名とパスワードが入力済みでなければなりません。<br>
+					ユーザー登録は不要です。画面右下の<i class="fa fa-gear"></i>設定ボタンを開き、ユーザー名、パスワード欄に好きな値を設定してください。<br>
+				</p>
+				<div><img src="../img/manual/02-03-01.jpg"></div>
+				<p>
+					「デッキを新規登録しました」と表示されれば登録は完了です。
+				</p>
+				<div><img src="../img/manual/02-03-02.jpg"></div>
+			</section>
+
+			<section>
+				<h3 id="idx2-4" class="sectionHeader"><i class="fa fa-wrench"></i>作成済みデッキを編集する</h3>
+				<p>
+					デッキを登録した後にデッキ一覧画面を開くと、登録したデッキの一覧が表示されます。<br>
+					編集したいデッキをクリックすると、デッキ編集画面に移動します。<br>
+					編集画面での操作方法は、デッキ作成画面と同じです。
+				</p>
+				<div><img src="../img/manual/02-04-01.jpg"></div>
+				<p>
+					デッキの編集が終わったら、保存ボタンをクリックします。<br>
+					「デッキを更新しました」と表示されれば、編集は完了です。
+				</p>
+			</section>
+			<section>
+				<h3 id="idx2-5" class="sectionHeader"><i class="fa fa-search"></i>デッキを探す</h3>
+				<p>
+					<a href="./deckShare.php">デッキシェアページ</a>から、<i class="fa fa-search"></i>探すボタンをクリックします。<br>
+				</p>
+				<div><img src="../img/manual/02-05-01.jpg"></div>
+				<p>
+					検索画面に移動したら、検索項目の設定を行います。<br>
+				</p>
+				<div><img src="../img/manual/02-05-02.jpg"></div>
+				<p class="dlTitle">▼検索項目</p>
+				<dl class="defList">
+					<dt>デッキ名</dt>
+					<dd>検索ワードがデッキ名に含まれるデッキがヒットします。空欄の場合は条件から除外されます。</dd>
+					<dt>育成タイプ</dt>
+					<dd>「指定なし」の場合、条件から除外されます。</dd>
+					<dt>対象高校</dt>
+					<dd>「指定なし」の場合、条件から除外されます。</dd>
+					<dt>イベキャラ</dt>
+					<dd>
+						選択したイベキャラを使用しているデッキがヒットします。空欄の場合は条件から除外されます。<br>
+						※Ver違いを指定する場合、そのVerを指定しないと検索にヒットしません。
+					</dd>
+					<dt>作者名</dt>
+					<dd>空欄の場合は条件から除外されます。</dd>
+					<dt>Twitter</dt>
+					<dd>空欄の場合は条件から除外されます。</dd>
+					<dt>お気に入りのみ</dt>
+					<dd>このチェックを入れると、自分がお気に入り登録したデッキのみが画面に表示されます。</dd>
+				</dl>
+				<p>検索で表示されるデッキは、検索条件に入力した全ての条件を満たすデッキになります。</p>
+				<p>条件を設定せずに検索した場合、登録されている全てのデッキを表示します。</p>
+				<p>「リセット」ボタンを押すことで、入力した条件をリセットします。</p>
+
+				<div><img src="../img/manual/02-05-03.jpg"></div>
+
+				<p>
+					設定が完了したら「検索」ボタンを押すと検索が行われます。<br>
+					検索条件に合致した検索条件フォーム下に表示されます。
+				</p>
+				<div><img src="../img/manual/02-05-04.jpg"></div>
+				<p>
+					詳細を見たいデッキをクリックすると、詳細ページに移動します。
+				</p>
+				<div><img src="../img/manual/02-05-05.jpg"></div>
+			</section>
+
+			<section>
+				<h3 id="idx2-6" class="sectionHeader"><i class="fa fa-search"></i>デッキをお気に入り登録する</h3>
+				<p>
+					デッキ詳細画面下部の「お気に入り」ボタンを押すことで、閲覧しているデッキをお気に入り登録できます。
+				</p>
+				<div><img src="../img/manual/02-06-01.jpg"></div>
+				<p>
+					「お気に入り登録しました」と表示されれば完了です。
+				</p>
+				<div><img src="../img/manual/02-06-02.jpg"></div>
+				<p>
+					お気に入り済みのデッキは、お気に入りボタンが黄色くなります。<br>
+					お気に入りボタンをもう一度押すと、お気に入り解除できます。<br>
+					※お気に入り機能を使うには、ユーザー名、パスワードの入力が必要です。
+				</p>
+
+			</section>
+
+			<section>
+				<h3 id="idx2-7" class="sectionHeader"><i class="fa fa-search"></i>デッキを共有する</h3>
+				<p>
+					デッキ作成(編集)画面下の公開用URLを使用して、作成したデッキを他サイトで公開することができます。
+				</p>
+				<div><img src="../img/manual/02-07-01.jpg"></div>
+				<p>
+					また、デッキ詳細画面のURLを使用して公開することもできます。
+				</p>
+				<div><img src="../img/manual/02-07-02.jpg"></div>
+
+			</section>
+
+		</section>
 
 
 	</main>
