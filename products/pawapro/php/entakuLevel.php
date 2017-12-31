bf1bd7c515a7999aa89833020d031fa55d8ac33b
hitsuji <hitsuji_sw@yahoo.co.jp>
2017-12-31 19:17:43 +0900

    円卓初期レベル計算機のロジック修正(2種練持ち用に調整)、グラフィカルに調整

--- products/pawapro/php/entakuLevel.php
+++ products/pawapro/php/entakuLevel.php
@@ -13,8 +13,8 @@
     $description = 'パワプロアプリ円卓高校の初期練習レベルを計算するツールです。';
     require_once './headInclude.php';
     ?>
-    <link rel="stylesheet" href="../css/entakuLevel.css">
-    <script src="../js/entakuLevel.js?ver20171220"></script>
+    <link rel="stylesheet" href="../css/entakuLevel.css?ver20171230">
+    <script src="../js/entakuLevel.js?ver20171230"></script>
 </head>
 
 <body>
@@ -25,44 +25,44 @@
             <h2><i class="fa fa-trophy" aria-hidden="true"></i>円卓高校練習レベル計算機</h2>
         </header>
         <section>
-            <header class="secHeader">■得意練習</header>
+            <header class="secHeader"><span>■得意練習</span><button class="allReset" onclick="entakuLevel.resetAll()">リセット</button></header>
 
             <div class="secMain">
                 <table class="modern eveCharaTable">
                     <tr>
                         <th></th>
-                        <th>メイン練習</th>
-                        <th>サブ練習</th>
+                        <th>メイン</th>
+                        <th>サブ</th>
                     </tr>
                     <tr>
                         <th>1</th>
-                        <td><?= makeCharaTypeList(); ?></td>
-                        <td><?= makeCharaTypeList(); ?></td>
+                        <td><img class="charaType" src="../img/entakuBlank.jpg"></td>
+                        <td><img class="charaType" src="../img/entakuBlank.jpg"></td>
                     </tr>
                     <tr>
                         <th>2</th>
-                        <td><?= makeCharaTypeList(); ?></td>
-                        <td><?= makeCharaTypeList(); ?></td>
+                        <td><img class="charaType" src="../img/entakuBlank.jpg"></td>
+                        <td><img class="charaType" src="../img/entakuBlank.jpg"></td>
                     </tr>
                     <tr>
                         <th>3</th>
-                        <td><?= makeCharaTypeList(); ?></td>
-                        <td><?= makeCharaTypeList(); ?></td>
+                        <td><img class="charaType" src="../img/entakuBlank.jpg"></td>
+                        <td><img class="charaType" src="../img/entakuBlank.jpg"></td>
                     </tr>
                     <tr>
                         <th>4</th>
-                        <td><?= makeCharaTypeList(); ?></td>
-                        <td><?= makeCharaTypeList(); ?></td>
+                        <td><img class="charaType" src="../img/entakuBlank.jpg"></td>
+                        <td><img class="charaType" src="../img/entakuBlank.jpg"></td>
                     </tr>
                     <tr>
                         <th>5</th>
-                        <td><?= makeCharaTypeList(); ?></td>
-                        <td><?= makeCharaTypeList(); ?></td>
+                        <td><img class="charaType" src="../img/entakuBlank.jpg"></td>
+                        <td><img class="charaType" src="../img/entakuBlank.jpg"></td>
                     </tr>
                     <tr>
                         <th>6</th>
-                        <td><?= makeCharaTypeList(); ?></td>
-                        <td><?= makeCharaTypeList(); ?></td>
+                        <td><img class="charaType" src="../img/entakuBlank.jpg"></td>
+                        <td><img class="charaType" src="../img/entakuBlank.jpg"></td>
                     </tr>
                 </table>
             </div>
@@ -80,21 +80,99 @@
         </section>
 
         <section>
-            <header class="secHeader">■練習レベル</header>
+            <header class="secHeader">
+               ■練習レベル
+                <select id="targetType" onchange="entakuLevel.update();">
+                    <option value="0">野手育成</option>
+                    <option value="1">投手育成</option>
+                </select>
+            </header>
             <div class="secMain">
-                <p>打撃：Lv<span class="dispLevel"></span></p>
-                <p>筋力：Lv<span class="dispLevel"></span></p>
-                <p>走塁：Lv<span class="dispLevel"></span></p>
-                <p>肩力：Lv<span class="dispLevel"></span></p>
-                <p class="noneDisp">球速：Lv<span class="dispLevel"></span></p>
-                <p class="noneDisp">ｺﾝﾄﾛｰﾙ：Lv<span class="dispLevel"></span></p>
-                <p class="noneDisp">ｽﾀﾐﾅ：Lv<span class="dispLevel"></span></p>
-                <p class="noneDisp">変化球：Lv<span class="dispLevel"></span></p>
-                <p>守備：Lv<span class="dispLevel"></span></p>
-                <p>精神：Lv<span class="dispLevel"></span></p>
+                <div class="divBatter">
+                    <div class="playerType">野手</div>
+                    <ul class="dispList dispListBatter">
+                        <li>
+                            <div><img src="../img/practice0.jpg"></div>
+                            <div class="lvArea">Lv<span class="dispLevel"></span></div>
+                        </li>
+                        <li>
+                            <div><img src="../img/practice1.jpg"></div>
+                            <div class="lvArea">Lv<span class="dispLevel"></span></div>
+                        </li>
+                        <li>
+                            <div><img src="../img/practice2.jpg"></div>
+                            <div class="lvArea">Lv<span class="dispLevel"></span></div>
+                        </li>
+                        <li>
+                            <div><img src="../img/practice3.jpg"></div>
+                            <div class="lvArea">Lv<span class="dispLevel"></span></div>
+                        </li>
+                        <li>
+                            <div><img src="../img/practice8.jpg"></div>
+                            <div class="lvArea">Lv<span class="dispLevel"></span></div>
+                        </li>
+                        <li>
+                            <div><img src="../img/practice9.jpg"></div>
+                            <div class="lvArea">Lv<span class="dispLevel"></span></div>
+                        </li>
+                    </ul>
+                </div>
+                <div class="divPitcher">
+                    <div class="playerType">投手</div>
+                    <ul class="dispList dispListPitcher">
+                        <li>
+                            <div><img src="../img/practice4.jpg"></div>
+                            <div class="lvArea">Lv<span class="dispLevel"></span></div>
+                        </li>
+                        <li>
+                            <div><img src="../img/practice5.jpg"></div>
+                            <div class="lvArea">Lv<span class="dispLevel"></span></div>
+                        </li>
+                        <li>
+                            <div><img src="../img/practice6.jpg"></div>
+                            <div class="lvArea">Lv<span class="dispLevel"></span></div>
+                        </li>
+                        <li>
+                            <div><img src="../img/practice7.jpg"></div>
+                            <div class="lvArea">Lv<span class="dispLevel"></span></div>
+                        </li>
+                        <li>
+                            <div><img src="../img/practice8.jpg"></div>
+                            <div class="lvArea">Lv<span class="dispLevel"></span></div>
+                        </li>
+                        <li>
+                            <div><img src="../img/practice9.jpg"></div>
+                            <div class="lvArea">Lv<span class="dispLevel"></span></div>
+                        </li>
+                    </ul>
+                </div>
+
             </div>
         </section>
     </main>
+
+    <div id="practiceModal" class="remodal" data-remodal-id="modal" data-remodal-options="hashTracking:false, closeOnCancel:false, closeOnConfirm:false, closeOnEscape:false">
+        <button class="remodal-close"  onclick="$.remodal.lookup[$('[data-remodal-id=modal]').data('remodal')].close()"></button>
+
+        <ul class="choicePraItems">
+            <li><img src="../img/practice0.jpg" data-number="0"></li>
+            <li><img src="../img/practice1.jpg" data-number="1"></li>
+            <li><img src="../img/practice2.jpg" data-number="2"></li>
+            <li><img src="../img/practice3.jpg" data-number="3"></li>
+            <li><img src="../img/practice4.jpg" data-number="4"></li>
+            <li><img src="../img/practice5.jpg" data-number="5"></li>
+            <li><img src="../img/practice6.jpg" data-number="6"></li>
+            <li><img src="../img/practice7.jpg" data-number="7"></li>
+            <li><img src="../img/practice8.jpg" data-number="8"></li>
+            <li><img src="../img/practice9.jpg" data-number="9"></li>
+            <li><img src="../img/practice999.jpg" data-number="999"></li>
+        </ul>
+
+        <div class="modalButton">
+            <button data-remodal-action="cancel" class="remodal-cancel" onclick="entakuLevel.resetPractice();">練習削除</button>
+            <button data-remodal-action="cancel" class="remodal-confirm" onclick="$.remodal.lookup[$('[data-remodal-id=modal]').data('remodal')].close()">Close</button>
+        </div>
+    </div>
 
 
     <?php include('./optionMenu.php'); ?>
